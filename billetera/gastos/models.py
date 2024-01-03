dcb53647bf9443f0f7c510881a0deb3d906e188d
Ontos <josemiguel.mercado17@gmail.com>
2024-01-03 02:40:13 -0300

    Le incluí el atributo 'moneda' al modelo gastos

--- billetera/gastos/models.py
+++ billetera/gastos/models.py
@@ -5,6 +5,7 @@ class Gasto(models.Model):
     descripcion = models.CharField(max_length=255)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
     fecha = models.DateTimeField(auto_now_add=True)  # Agrega este campo para la fecha de creación.
+    moneda = models.CharField(max_length=10, default='pesos')
 
     def __str__(self):
         return self.descripcion
