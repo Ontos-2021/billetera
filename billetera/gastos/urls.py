9cc59be2b993e58fd44b3c5d0c5cf66bede3ac5f
Ontos <josemiguel.mercado17@gmail.com>
2024-01-04 23:18:50 -0300

    Modifique la URL de lista gasto para hacerla HOME

--- billetera/gastos/urls.py
+++ billetera/gastos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lista/', views.lista_gastos, name='lista_gastos'),
+    path('', views.lista_gastos, name='lista_gastos'),
     path('crear/', views.crear_gasto, name='crear_gasto'),
     path('editar/<int:id>/', views.editar_gasto, name='editar_gasto'),
     path('eliminar/<int:id>/', views.eliminar_gasto, name='eliminar_gasto'),
