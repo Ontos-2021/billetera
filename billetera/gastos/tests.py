2d4af87b69c34e33ee25a52332658149ed0686c1
Ontos <josemiguel.mercado17@gmail.com>
2024-10-27 06:23:20 -0300

    Integrando diseño de claude

--- billetera/gastos/tests.py
+++ billetera/gastos/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#Escribir test que corrobore que el admin puede editar, y eliminar gastos accediendo a la url correspondiente
+# Hay que considerar que no puede eliminar el id de un gasto que no es suyo (?)
