80df9751f73f7144eb7441045532e04fac2aded1
Ontos <josemiguel.mercado17@gmail.com>
2024-10-30 09:38:14 -0300

    Fixing

--- billetera/billetera/settings.py
+++ billetera/billetera/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver.nostatic',
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'gastos',
     'usuarios',
